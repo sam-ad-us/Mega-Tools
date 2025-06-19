4f41ae2e89586d11653ea561098a1cedc201cd55
Sam ad-us <samadus2005@gmail.com>
2025-06-19 22:42:32 +0530

    Add files via upload

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,5 +1,18 @@
-fetch('../footer.html')
-  .then(res => res.text())
-  .then(data => {
-    document.body.insertAdjacentHTML('beforeend', data);
-  });
+function loadFooter() {
+  fetch('footer.html')
+    .then(res => {
+      if (!res.ok) throw new Error();
+      return res.text();
+    })
+    .then(data => {
+      document.body.insertAdjacentHTML('beforeend', data);
+    })
+    .catch(() => {
+      fetch('../footer.html')
+        .then(res => res.text())
+        .then(data => {
+          document.body.insertAdjacentHTML('beforeend', data);
+        });
+    });
+}
+loadFooter();
